27c17ab8f6fc3d22add7fcc2e49f7ff6c1841729
Boris Quiroz <borisquiroz@pm.me>
2025-03-20 06:56:50 -0300

    Add some missing commits

--- init.lua
+++ init.lua
@@ -27,7 +27,6 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-
 local plugins = {
     { "preservim/nerdtree" },
     { "github/copilot.vim" },
@@ -44,14 +43,54 @@ local plugins = {
     { "vim-airline/vim-airline" },
     { "vim-airline/vim-airline-themes" },
     { "tpope/vim-fugitive" },
-    { "hashivim/vim-terraform" },
-    { "sheerun/vim-polyglot" },
     { "EdenEast/nightfox.nvim" },
+    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
     { "nvim-telescope/telescope.nvim", tag = "0.1.8",
        dependencies = { 
            "nvim-lua/plenary.nvim"
        }
     },
+    { "nvim-treesitter/nvim-treesitter", build= ":TSUpdate" },
+    { "startup-nvim/startup.nvim",
+        dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim", "nvim-telescope/telescope-file-browser.nvim" },
+        config = function()
+            require "startup".setup()
+        end
+    },
+    { 'nvimdev/dashboard-nvim',
+        event = 'VimEnter',
+        config = function()
+            require('dashboard').setup({
+                theme = 'hyper',
+                config = {
+                    week_header = {
+                        enable = true,
+                    },
+                },
+            })
+        end,
+        dependecies = { {'nvim-tree/nvim-web-devicons'}}
+    },
+    {
+        "magatti/auto-session",
+        config = function()
+            require("auto-session").setup({
+                auto_session_suppressed_dirs = { "~", "~/Projects", "~/Downloads", "/" },
+                session_lens = {
+                    load_on_setup = true,
+                    previewer = false,
+                    mappings = {
+                        delete_session = { "i", "<C-D>" },
+                        alternate_session = { "i", "<C-S>" },
+                        copy_session = { "i", "<C-Y>" },
+                    },
+                    vim.keymap.set("n", "<Leader>ls", require("auto-session.session-lens").search_session, {
+                        noremap = true,
+                    }),
+                },
+            })
+        end,
+    }
 }
 local opts = {}
 
@@ -61,6 +100,14 @@ require("lazy").setup(plugins, opts)
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+
+-- Treesitter
+local config = require('nvim-treesitter.configs')
+config.setup {
+  ensure_installed = {"lua", "yaml", "json", "dockerfile", "python", "hcl"},
+  highlight = { enable = true },
+  indent = { enable = true },
+}
 
 -- NERDTree configuration
 vim.api.nvim_set_keymap('n', '<C-n>', ':NERDTreeToggle<CR>', { noremap = true, silent = true })
@@ -97,34 +144,34 @@ for _, lsp in ipairs(servers) do
 end
 
 -- nvim-cmp setup
-local cmp = require'cmp'
-local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-
-cmp.setup({
-  snippet = {
-    expand = function(args)
-      require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-    end,
-  },
-  mapping = {
-    ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-    ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-    ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-    ['<C-e>'] = cmp.mapping({
-      i = cmp.mapping.abort(),
-      c = cmp.mapping.close(),
-    }),
-    ['<C-k>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-    ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' }),
-    ['<S-Tab>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 's' }),
-  },
-  sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-  }, {
-    { name = 'buffer' },
-  })
-})
+--local cmp = require'cmp'
+--local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+--
+--cmp.setup({
+--  snippet = {
+--    expand = function(args)
+--      require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+--    end,
+--  },
+--  mapping = {
+--    ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+--    ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+--    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+--    ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+--    ['<C-e>'] = cmp.mapping({
+--      i = cmp.mapping.abort(),
+--      c = cmp.mapping.close(),
+--    }),
+--    ['<C-k>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+--    ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' }),
+--    ['<S-Tab>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 's' }),
+--  },
+--  sources = cmp.config.sources({
+--    { name = 'nvim_lsp' },
+--  }, {
+--    { name = 'buffer' },
+--  })
+--})
 
 -- Setup lspconfig.
 local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
@@ -160,7 +207,10 @@ vim.g.copilot_no_tab_map = true
 require('nvim-autopairs').setup{}
 
 -- Theme configuration
-vim.cmd('colorscheme nordfox')
+require("catppuccin").setup({
+    term_colors = true,
+})
+vim.cmd('colorscheme catppuccin-macchiato')
 vim.g.airline_powerline_fonts = 1
 vim.g.airline_theme = 'deus'
 
