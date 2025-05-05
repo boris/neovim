4e6a7778ae1f64d4a68a65a51a76b2867ea86114
Boris Quiroz <borisquiroz@pm.me>
2025-05-05 07:01:24 -0400

    Add git gutter

--- lua/plugins/tools.lua
+++ lua/plugins/tools.lua
@@ -19,4 +19,37 @@ return {
       cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
     end,
   },
+  -- Git gutter plugin
+  {
+    "lewis6991/gitsigns.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      require("gitsigns").setup({
+        signs = {
+          add          = { text = "│" },
+          change       = { text = "│" },
+          delete       = { text = "_" },
+          topdelete    = { text = "‾" },
+          changedelete = { text = "~" },
+        },
+        current_line_blame = false,
+        on_attach = function(bufnr)
+          local gs = package.loaded.gitsigns
+          local map = function(mode, l, r, desc)
+            vim.keymap.set(mode, l, r, { buffer = bufnr, desc = desc })
+          end
+
+          -- Navigation
+          map("n", "]c", function() gs.next_hunk() end, "Next Hunk")
+          map("n", "[c", function() gs.prev_hunk() end, "Prev Hunk")
+
+          -- Actions
+          map("n", "<leader>gs", gs.stage_hunk, "Stage Hunk")
+          map("n", "<leader>gr", gs.reset_hunk, "Reset Hunk")
+          map("n", "<leader>gp", gs.preview_hunk, "Preview Hunk")
+          map("n", "<leader>gb", gs.toggle_current_line_blame, "Toggle Line Blame")
+        end,
+      })
+    end,
+  },
 }
